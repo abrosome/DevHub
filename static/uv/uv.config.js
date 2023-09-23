953181e1380ebafe0e72c35cd9113846e9696e83
Darian <116446349+justDarian@users.noreply.github.com>
2023-09-22 22:52:26 -0400

    Add files via upload

---
+++ static/uv/uv.config.js
@@ -0,0 +1,32 @@
+self.__uv$config = {
+    prefix: "/~/",
+    bare: "https://tomp.app/",
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: "/uv/uv.handler.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
+  };
+
+// loads eruda (js inspect)
+fetch("https://cdn.jsdelivr.net/npm/eruda").then(r => r.text()).then(r => eval(r))
+
+setTimeout(() => {
+  eruda.init()
+
+erudaInt = setInterval(() => {
+  if (document.getElementById("eruda").shadowRoot) {
+    const div = document.getElementById("eruda").shadowRoot.querySelector(".eruda-entry-btn");
+    if (div) {
+      div.style.left = "auto";
+      div.style.right = "0";
+      div.style.top = "auto";
+      div.style.bottom = "0";
+      div.style.position = "fixed";
+    }
+  }
+}, 100);
+
+}, 250);
+
